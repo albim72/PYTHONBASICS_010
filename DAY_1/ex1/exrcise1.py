6708a5b237d79c3fee56171f4e30d82e7e15da5f
albim72 <marcin.albiniak72@gmail.com>
2025-10-03 14:24:57 +0200

    Create exrcise1.py

---
+++ DAY_1/ex1/exrcise1.py
@@ -0,0 +1,16 @@
+# Lista tytułów z duplikatami
+raw_titles = [
+    "Dune", "Foundation", "Dune", "Neuromancer",
+    "Snow Crash", "Foundation", "Hyperion", "Dune"
+]
+
+# Lista krotek (ISBN, autor)
+isbn_author_pairs = [
+    ("9780441013593", "Frank Herbert"),
+    ("9780553293357", "Isaac Asimov"),
+    ("9780441569595", "William Gibson"),
+    ("9780553380958", "Dan Simmons")
+]
+
+# Zbiór książek wypożyczonych
+loaned_titles = {"Neuromancer", "Hyperion"}
