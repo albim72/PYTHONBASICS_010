467bdf017beb9847697d38fd27952167c5e8eae2
albim72 <marcin.albiniak72@gmail.com>
2025-10-06 09:47:32 +0200

    Create python_builtin_functions.py

---
+++ DAY_2/python_builtin_functions.py
@@ -0,0 +1,24 @@
+#standard libraries imports
+#third-party imports
+#local application imports
+
+from functools import reduce
+
+students = [
+    {"name":"Alice","scores":[80,90,100]},
+    {"name":"Bob","scores":[60,70,80]},
+    {"name":"Charlie","scores":[90,100,100]},
+    {"name":"Dan","scores":[45,33,12]},
+    {"name":"Ann","scores":[67,99,45]},
+]
+
+#map function, list, lambda
+#first parameter of map i function mapping values for every keys form list disctionaries(students)
+name_and_avg = list(map(lambda s: (s["name"], sum(s["scores"]) / len(s["scores"])), students))
+print(f"name_and_avg: {name_and_avg}")
+
+#filter function - keep only students with average >= 75
+#filter(function -> condition,data) -> result -> (name,avg)
+
+passed = list(filter(lambda t: t[1] >= 75, name_and_avg))
+print(f"passed: {passed}")
