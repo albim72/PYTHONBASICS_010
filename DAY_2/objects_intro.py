bde3c576385b3719099e7d5713c55544bd68fc09
albim72 <marcin.albiniak72@gmail.com>
2025-10-06 13:34:05 +0200

    Update objects_intro.py

--- DAY_2/objects_intro.py
+++ DAY_2/objects_intro.py
@@ -15,3 +15,28 @@ print(car1.model)
 car2 = Car("Mercedes", "B-Class")
 print(car2.brand)
 print(car2.model)
+
+print("_"*60)
+#method inside a class
+class Rectangle:
+    _color = "red"
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def area(self):
+        return self.a * self.b
+
+    #getter function
+    def color(self):
+        return self._color
+
+    #setter function
+    def change_color(self, new_color):
+        self._color = new_color
+
+r1 = Rectangle(10, 20)
+print(r1.area())
+print(r1.color())
+r1.change_color("blue")
+print(r1.color())
