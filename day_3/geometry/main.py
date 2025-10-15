8cac18be8439f06e03cba86c7c30d6f8bb18791a
albim72 <marcin.albiniak72@gmail.com>
2025-10-15 11:18:12 +0200

    Update main.py

--- day_3/geometry/main.py
+++ day_3/geometry/main.py
@@ -1,6 +1,7 @@
 from triangle import Triangle
 from rectangle import Rectangle
 from trapezoid import Trapezoid
+from cicrle import Circle
 
 tr = Triangle(3.6,7.2)
 print(f"The area of {tr.__class__.__name__} is {tr.calculate_area():.2f} cm2")
@@ -14,3 +15,5 @@ print(f"The area of {r2.__class__.__name__} is {r2.calculate_area():.2f} cm2")
 tp = Trapezoid(6.8,3.4,5.6)
 print(f"The area of {tp.__class__.__name__} is {tp.calculate_area():.2f} cm2")
 
+cr = Circle(4.5)
+print(f"The area of {cr.__class__.__name__} is {cr.calculate_area():.2f} cm2")
