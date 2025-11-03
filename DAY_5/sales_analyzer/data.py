5b554df85fb4dcfdd483e907b94060747e53b0b1
albim72 <marcin.albiniak72@gmail.com>
2025-11-03 11:05:24 +0100

    Create data.py

---
+++ DAY_5/sales_analyzer/data.py
@@ -0,0 +1,6 @@
+SLAES_DATA = [
+    {"product":"Laptop","price":1200,"quantity":3},
+    {"product":"Mouse","price":25,"quantity":20},
+    {"product":"Keyboard","price":75,"quantity":10},
+    {"product":"Monitor","price":320,"quantity":5},
+]
